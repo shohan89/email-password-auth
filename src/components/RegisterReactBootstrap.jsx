615a7804a8ed874061c1185f457b2da4619819c3
Shohan Sharoar <maffin.killer007@gmail.com>
2023-08-20 20:38:34 +0600

    added onsubmit event handler

--- src/components/RegisterReactBootstrap.jsx
+++ src/components/RegisterReactBootstrap.jsx
@@ -3,18 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const RegisterReactBootstrap = () => {
+    const handleRegister = ( e ) =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        console.log( email );
+    }
     return (
         <div className='w-50 mx-auto'>
             <h3 className='text-primary mt-5 mb-3'>Please Register</h3>
-            <Form>
+            <Form onSubmit={ handleRegister }>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
